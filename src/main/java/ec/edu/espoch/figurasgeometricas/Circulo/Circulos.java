b5093031415e10e3173deb5f0eeb9a82a10f6392
Lizbeth-girl <saezlizbethe6@gamail.com>
2024-11-27 12:50:58 -0500

    implementacion de las clases cuadrado y circulo

--- src/main/java/ec/edu/espoch/figurasgeometricas/Circulo/Circulos.java
+++ src/main/java/ec/edu/espoch/figurasgeometricas/Circulo/Circulos.java
@@ -9,5 +9,28 @@ package ec.edu.espoch.figurasgeometricas.Circulo;
  * @author PERSONAL
  */
 public class Circulos {
+  double pi = 3.1416;
     
+    public double radioCirculo;
+    
+    public double areaCirculo;
+    public double perimetroCirculo;
+
+    public Circulos() {
+    }
+
+    public Circulos(double radioCirculo) {
+        this.radioCirculo = radioCirculo;  
+    }
+    
+    public void imprimir(){
+        System.out.println("El valor de radio es: " + radioCirculo);
+}
+    
+    public double calcularArea(){
+    return (radioCirculo*radioCirculo)/pi;
+    }
+    public double calcularPerimetro(){
+    return 2*radioCirculo*pi;    
+    }   
 }
--- src/main/java/ec/edu/espoch/figurasgeometricas/Cuadrado/Cuadrados.java
+++ src/main/java/ec/edu/espoch/figurasgeometricas/Cuadrado/Cuadrados.java
@@ -9,5 +9,28 @@ package ec.edu.espoch.figurasgeometricas.Cuadrado;
  * @author PERSONAL
  */
 public class Cuadrados {
+   
+      public double ladoCuadrado;
     
+    
+ public Cuadrados(double cuadrado) {
+        this.ladoCuadrado = cuadrado;
+    }
+    
+    public void imprimir(){
+    
+        System.out.println("Los lados del cuadrado son: " + ladoCuadrado);
+    }
+    
+    public double perimetroCuadrado(){
+     
+        
+        return ladoCuadrado*ladoCuadrado;
+    }
+    
+    public double areaCuadrado(){
+    
+        return ladoCuadrado*ladoCuadrado*ladoCuadrado*ladoCuadrado;
+   
+}
 }
