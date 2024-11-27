f3cd7654323c7be4eb6e8388fc335f41af223d8d
Lizbeth-girl <saezlizbethe6@gamail.com>
2024-11-27 10:59:14 -0500

    Creación del proyecto

---
+++ src/main/java/ec/edu/espoch/figurasgeometricas/FigurasGeometricas.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package ec.edu.espoch.figurasgeometricas;
+
+import ec.edu.espoch.figurasgeometricas.Circulo.Circulos;
+import ec.edu.espoch.figurasgeometricas.Cuadrado.Cuadrados;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class FigurasGeometricas {
+
+    public static void main(String[] args) {
+
+        Circulos objCirculo = new Circulos();
+
+        objCirculo.radioCirculo = 4.5;
+        objCirculo.imprimir();
+
+        System.out.println("El area del circulo es: " + objCirculo.calcularArea());
+
+        System.out.println("El perimetro es: " + objCirculo.calcularPerimetro());
+
+        Cuadrados objCuadrado = new Cuadrados(2);
+
+        objCuadrado.ladoCuadrado = 5;
+
+        objCuadrado.imprimir();
+
+        objCuadrado.perimetroCuadrado();
+
+        System.out.println("El perimetro cuadrado es: " + objCuadrado.perimetroCuadrado());
+
+        objCuadrado.areaCuadrado();
+
+        System.out.println("El area del cuadrado es: " + objCuadrado.areaCuadrado());
+
+    }
+
+}
