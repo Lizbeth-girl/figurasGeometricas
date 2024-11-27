d7edabe9e8177180c111023fe308c4c5825cc55c
Stalyn0101 <yixon11@gmail.com>
2024-11-27 13:19:52 -0500

    variables

--- src/main/java/ec/edu/espoch/figurasgeometricas/Triangulo/Triangulo.java
+++ src/main/java/ec/edu/espoch/figurasgeometricas/Triangulo/Triangulo.java
@@ -9,5 +9,47 @@ package ec.edu.espoch.figurasgeometricas.Triangulo;
  * @author USER
  */
 public class Triangulo {
-    
+     public double Base;
+    public double Altura;
+
+    public Triangulo() {
+    }
+
+    public Triangulo(double Base, double Altura) {
+        this.Base = Base;
+        this.Altura = Altura;
+
+    }
+
+    public void imprimir() {
+
+        System.out.println("La base del triangulo es: " + Base);
+        System.out.println("La altura del triangulo es: " + Altura);
+
+    }
+
+    public double calcularArea() {
+        return (Base * Altura) / 2;
+    }
+
+
+    public double hipotenusa() {
+        return Math.sqrt(Math.pow(Base, 2) + Math.pow(Altura, 2));
+    }
+
+    public String Triangulo() {
+
+        if (Base == Altura && Base == hipotenusa()) {
+            return "Equilatero";
+        } else {
+            if (Base == Altura || Base == hipotenusa() || Altura == hipotenusa()) {
+                return "Isosceles";
+            } else {
+                return "Escaleno";
+            }
+        }
+    }
+
 }
+
+
